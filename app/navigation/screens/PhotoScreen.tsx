b1598dcf884fedf455878720f4bb7a9fb58f06b4
chicoumbetov <shyngysmaaan@gmail.com>
2023-06-15 22:02:17 +0200

    Created a new Expo app

--- app/navigation/screens/PhotoScreen.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PhotoScreen = () => {
-  return <div>PhotoScreen</div>;
-};
-
-export default PhotoScreen;
--- components/Icon/typegenerator.js
+++
@@ -1,2 +0,0 @@
-const config = require('./selection');
-console.log(config.icons.map((icon) => `"${icon.properties.name}"`).join(' | '))
