e7265b5edc6e0224c2c414eedfb2ee0ac3d4ccc0
chicoumbetov <shyngysmaaan@gmail.com>
2023-06-17 12:00:49 +0200

    :lipstick: add colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,28 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
+  gradientForm: "#A376F1",
+  primary: "#7d5fff",
+  white: "#FFFFFF",
+  bgColor: "#82ccdd",
+  warning: "#f0d500",
+  danger: "#FF0D0E",
+  gray: "#666666",
+  grayLight: "#ccc",
+  black: "#0a0a0a",
 };
