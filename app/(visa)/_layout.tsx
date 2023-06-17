1733811ec1f1ebb613fd05b9c003ac37eecf0f9d
chicoumbetov <shyngysmaaan@gmail.com>
2023-06-17 13:29:45 +0200

    :sparkles: visa folder Finance

---
+++ app/(visa)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Drawer from "../../components/Drawer/Drawer";
+
+const _layout = () => {
+  return (
+    <Drawer>
+      <Drawer.Screen name="Finance" />
+    </Drawer>
+  );
+};
+
+export default _layout;
