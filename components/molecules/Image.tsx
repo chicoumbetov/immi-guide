2121abc2779d44a804063dc8cfc4141419dbef48
chicoumbetov <shyngysmaaan@gmail.com>
2023-07-06 17:17:53 +0200

    image comps created

---
+++ components/molecules/Image.tsx
@@ -0,0 +1,128 @@
+/**
+ *
+ *
+ * @author: David-Julian Buch
+ */
+
+import * as _ from "lodash";
+import * as React from "react";
+import {
+  Animated,
+  ImageStyle,
+  Platform,
+  Image as RNImage,
+  ImageProps as RNImageProps,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from "react-native";
+
+import { useEffect, useState } from "react";
+import BlurView from "../BlurView";
+import CacheManager from "../CacheManager";
+import { useHasChanged } from "../utils/CustomHooks";
+
+export type ImageProps = Omit<RNImageProps, "source" | "style"> & {
+  style: ViewStyle;
+  preview?: string;
+  uri: string;
+};
+
+export default function Image(props: ImageProps): JSX.Element {
+  const { preview, style, defaultSource, uri, ...otherProps } = props;
+  const [uriState, setUriState] = useState<string | undefined>();
+  const [intensity] = useState(new Animated.Value(100));
+  const previousState = useHasChanged(uriState);
+
+  useEffect(() => {
+    let isMounted = true;
+    (async () => {
+      if (uri) {
+        // only cache non local uri's
+        let path: string | undefined = uri;
+        if (uri.indexOf("file://") <= -1) {
+          const entry = CacheManager.get(uri);
+          path = await entry.getPath();
+        }
+        if (isMounted) {
+          setUriState(path);
+        }
+      }
+    })();
+    return () => {
+      isMounted = false;
+    };
+  }, [uri]);
+
+  useEffect(() => {
+    if (preview && previousState === undefined) {
+      Animated.timing(intensity, {
+        duration: 300,
+        toValue: 0,
+        useNativeDriver: Platform.OS === "android",
+      }).start();
+    }
+  }, [uriState]);
+
+  const hasPreview = !!preview;
+  const isImageReady = !!uriState;
+  const computedStyle = StyleSheet.flatten<ImageStyle>([
+    StyleSheet.absoluteFill,
+    _.transform(
+      _.pickBy(
+        StyleSheet.flatten(style),
+        (_value, key) => propsToCopy.indexOf(key) !== -1
+      ),
+      (result, value, key) =>
+        Object.assign(result, {
+          [key]:
+            typeof value === "number" ? value - (style?.borderWidth || 0) : 0,
+        })
+    ),
+  ]);
+
+  return (
+    <View style={style}>
+      {defaultSource && !hasPreview && !isImageReady && (
+        <RNImage
+          {...otherProps}
+          source={defaultSource}
+          resizeMode="cover"
+          style={[
+            computedStyle,
+            {
+              width: undefined,
+              height: undefined,
+              flex: 1,
+              alignSelf: "stretch",
+            },
+          ]}
+        />
+      )}
+      {hasPreview && (
+        <RNImage
+          source={{ uri: preview }}
+          resizeMode="cover"
+          style={computedStyle}
+          blurRadius={Platform.OS === "android" ? 0.5 : 0}
+        />
+      )}
+      {isImageReady && (
+        <RNImage
+          {...otherProps}
+          source={{ uri: uriState }}
+          style={computedStyle}
+        />
+      )}
+      {hasPreview && <BlurView style={computedStyle} {...{ intensity }} />}
+    </View>
+  );
+}
+
+const propsToCopy = [
+  "borderRadius",
+  "borderBottomLeftRadius",
+  "borderBottomRightRadius",
+  "borderTopLeftRadius",
+  "borderTopRightRadius",
+];
---
+++ components/molecules/S3Image.tsx
@@ -0,0 +1,78 @@
+/**
+ *
+ *
+ * @author: David-Julian Buch
+ */
+
+import * as React from "react";
+import { Platform, View } from "react-native";
+
+import { useEffect, useState } from "react";
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+import Storage from "@aws-amplify/storage";
+import ActivityIndicator from "../atoms/ActivityIndicator";
+import Image, { ImageProps } from "./Image";
+
+type S3ImageProps = Omit<ImageProps, "uri"> & {
+  s3key: string;
+};
+
+export default function S3Image(props: S3ImageProps): JSX.Element {
+  const { s3key, ...otherProps } = props;
+  const [uri, setUri] = useState<false | string>(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    if (uri) {
+      setUri(false);
+    }
+    (async () => {
+      let found = false;
+      if (Platform.OS !== "web") {
+        // on regarde s'il existe encore le fichier dans le cache d'upload
+        const uuid = extractUUID(s3key);
+        const value = `@S3Object_${uuid}`;
+        /*
+        const stored = await AsyncStorage.getItem(value);
+        if (stored) {
+          const infos = JSON.parse(stored);
+          if (isMounted) {
+            found = true;
+            setUri(FileSystem.documentDirectory + waitingDirectory + infos.key);
+          }
+        }
+        */
+      }
+      if (!found) {
+        const url = await Storage.get(s3key);
+        if (isMounted) {
+          setUri(url as string);
+        }
+      }
+    })();
+    return () => {
+      isMounted = false;
+    };
+  }, [s3key]);
+
+  if (!uri) {
+    return (
+      <View
+        style={[
+          otherProps.style,
+          { flex: 1, justifyContent: "center", alignItems: "center" },
+        ]}
+      >
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  return <Image {...otherProps} uri={uri} />;
+}
+
+const extractUUID = (key: string) => {
+  const lastPathItem = key.split("/").pop();
+  const uuid = lastPathItem?.split(".").shift();
+  return uuid;
+};
